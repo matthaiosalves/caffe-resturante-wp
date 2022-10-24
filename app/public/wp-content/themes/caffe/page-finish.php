756f753e369d14e801ef19e48693be00ddb9c1e9
Matheus <lucas24417@gmail.com>
2022-10-24 20:07:43 -0300

    ✨ added to checkout page

---
+++ app/public/wp-content/themes/caffe/page-finish.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Finish */ ?>
+
+<?php get_header(); ?>
+<?php get_template_part( 'template-parts/products/component', 'finish' ); ?>
+<?php
+get_footer();
---
+++ app/public/wp-content/themes/caffe/template-parts/products/component-finish.php
@@ -0,0 +1,47 @@
+
+<section class="section--finish mt-5">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="content">
+        <div class="col-lg-6 boxText">
+          <h2 class="title">Uhu! Pedido confirmado</h2>
+          <p class="description">Agora é só aguardar que logo o café chegará até você</p>
+
+          <div class="descriptionDelivery">
+            <div class="street">
+              <div class="image">
+                <img src="<?php echo get_template_directory_uri(); ?>/src/img/Finish/local.svg" alt="Imagem de Localização">
+              </div>
+              <div class="description">
+                <p>Entrega em <strong>Rua João Daniel Martinelli, 102</strong><br> Farrapos - Porto Alegre, RS</p>
+              </div>
+            </div>
+            <div class="timer">
+              <div class="image">
+                <img src="<?php echo get_template_directory_uri(); ?>/src/img/Finish/timer.svg" alt="Imagem de Tempo">
+              </div>
+              <div class="description">
+                <p>Previsão de entrega<br>
+                  <strong> 20 min - 30 min </strong>
+                </p>
+              </div>
+            </div>
+            <div class="payment">
+              <div class="image">
+                <img src="<?php echo get_template_directory_uri(); ?>/src/img/Finish/money.svg" alt="Imagem de Pagamento">
+              </div>
+              <div class="description">
+                <p>Pagamento na entrega <br>
+                  <strong>Cartão de Crédito</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 boxImage">
+          <img src="<?php echo get_template_directory_uri(); ?>/src/img/Finish/moto.svg" alt="Imagem de uma Moto do Entregador">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
