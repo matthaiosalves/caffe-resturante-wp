ede4eeffe540455415b69e76241249f5724e4b0b
Matheus <lucas24417@gmail.com>
2022-10-24 12:26:59 -0300

    ✨ added the automatic zip code, button to select the type of payment

---
+++ app/public/wp-content/themes/caffe/src/js/payment.js
@@ -0,0 +1,8 @@
+if($('.section--single-content')){
+  $('a.btn-payment').click(function(){ 
+    $('a.btn-payment').children().hide();
+    $('a.btn-payment').removeAttr('style');
+    $(this).css({"background":"var(--PurpeLight)","color":"var(--Blue900)","border-radius":"6px", "border": "1px solid var(--Purple200)" });
+    $(this).children().show();
+  });
+}
---
+++ app/public/wp-content/themes/caffe/template-parts/products/component-payment.php
@@ -0,0 +1,87 @@
+<section class="section--single-content mt-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 boxCep">
+          <div class="row">
+            <div class="col-lg-12 boxTitle">
+              <h4 class="title">Complete seu pedido</h4>
+            </div>
+          </div>
+          <div class="card">
+            <div class="content">
+              <div class="boxAddress">
+                <img src="src/img/Single/address.svg" alt="Icone de Localização">
+                <span>Endereço de Entrega</span>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+                <div class="input-group mb-3 cep">
+                  <input type="text" class="form-control" placeholder="CEP" aria-label="CEP" aria-describedby="basic-addon1" maxlength="9" id="cep">
+                </div>
+                <div class="input-group mb-3 street">
+                  <input type="text" class="form-control" placeholder="Rua" aria-label="Rua" aria-describedby="basic-addon1" id="logradouro">
+                </div>
+                <div class="details mb-3">
+                  <div class="input-group bairro">
+                    <input type="text" class="form-control" placeholder="Bairro" aria-label="Bairro" aria-describedby="basic-addon1" id="bairro">
+                  </div>
+                  <div class="input-group city">
+                    <input type="text" class="form-control" placeholder="Cidade" aria-label="Cidade" aria-describedby="basic-addon1" id="localidade">
+                  </div>
+                  <div class="input-group uf">
+                    <input type="text" class="form-control" placeholder="UF" aria-label="UF" aria-describedby="basic-addon1" id="uf">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card mt-3 mb-3">
+            <div class="content">
+              <div class="boxPayment">
+                <img src="src/img/Single/payment.svg" alt="Icone de Pagamento">
+                <span>Pagamento</span>
+                <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>´
+              </div>
+              <div class="methodPayment">
+                <ul>
+                  <li><a class="btn-payment"><img src="src/img/Single/credit-card.svg" alt="Cartão de Crédito" style="display: none;" />&nbsp;Cartão de crédito</a></li>
+                  <li><a class="btn-payment"><img src="src/img/Single/debit-card.svg" alt="Cartão de Débito" style="display: none;" />&nbsp;cartão de débito</a></li>
+                  <li><a class="btn-payment"><img src="src/img/Single/money.svg" alt="Dinheiro" style="display: none;" />&nbsp;dinheiro</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 boxContentSelect">
+          <div class="row">
+            <div class="col-lg-12 boxTitle">
+              <h4 class="title">Cafés selecionados</h4>
+            </div>
+          </div>
+          <div class="card">
+            <div class="content">
+              <div class="boxImage">
+                <img src="src/img/Header/caffe.png" alt="Logo do Café selecionado">
+                <span class="title">Expresso Tradicional</span>
+                <span class="price">R$ 9,90</span>
+              </div>
+              <hr>
+              <div class="boxPrice">
+                <div class="priceItem">
+                  <span class="title">Total de itens</span>
+                  <span class="price">R$ 29,70</span>
+                </div>
+                <div class="priceDelivery">
+                  <span class="title">Entrega</span>
+                  <span class="price">R$ 3,50</span>
+                </div>
+                <div class="priceTotal">
+                  <span class="title">Total</span>
+                  <span class="price">R$ 33,20</span>
+                </div>
+              </div>
+              <a href="" class="btn btnDelivery">Confirmar pedido</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
